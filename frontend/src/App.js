f374c95b94e7bff4c4ace8dfb4693237767b8cbc
SeongEun <bongfam2@naver.com>
2024-11-07 03:53:34 +0900

    Test 2. CI/CD 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,9 +15,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn Reactttttttttttt
-        </a>
+        ></a>
       </header>
     </div>
   );
